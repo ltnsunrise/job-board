8b8db6f80066251b4462460b5cd15492570299ad
nhanlt-smartosc <nhanlt@smartosc.com>
2025-06-24 14:08:18 +0700

    add shadcn + sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,37 @@
-import Image from "next/image";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import AppSidebarClient from "./_AppSidebarClient";
 
 export default function HomePage() {
-  return <div>home</div>;
+  return (
+    <SidebarProvider className="overflow-y-hidden">
+      <AppSidebarClient>
+        <Sidebar collapsible="icon" className="overflow-hidden">
+          <SidebarHeader className="flex-row">
+            <SidebarTrigger />
+            <span className="text-xl text-nowrap">WDS Jobs</span>
+          </SidebarHeader>
+          <SidebarContent></SidebarContent>
+          <SidebarFooter>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>gfg</SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarFooter>
+        </Sidebar>
+
+        <main className="flex-1 overflow-y-auto">fdsfs</main>
+      </AppSidebarClient>
+    </SidebarProvider>
+  );
 }
